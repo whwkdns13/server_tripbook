20ed02fc94ac33f0475f6687f8744ca56bf2dbdd
Jneck <91073404+Jneck@users.noreply.github.com>
2022-01-19 22:46:57 +0900

    Update userDao.js
    select user()수정

--- src/app/User/userDao.js
+++ src/app/User/userDao.js
@@ -4,8 +4,8 @@
 // 모든 유저 조회
 async function selectUser(connection) {
   const selectUserListQuery = `
-                SELECT email, nickname 
-                FROM UserInfo;
+                SELECT * 
+                FROM user;
                 `;
   const [userRows] = await connection.query(selectUserListQuery);
   return userRows;
