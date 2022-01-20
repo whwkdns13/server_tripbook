89116e3d60ddc6e5edc87fd9b1e38ed19402872d
Jneck <parklim2254@gmail.com>
2022-01-21 00:13:48 +0900

    TripCourse 생성

---
+++ src/app/TripCourse/courseRoute.js
@@ -0,0 +1,30 @@
+module.exports = function(app){
+    const user = require('./userController');
+    const jwtMiddleware = require('../../../config/jwtMiddleware');    
+    const course = require('./courseController.js');
+
+
+    // 0. 테스트 API
+    app.get('/app/test/course', course.getTest);
+
+    
+    
+    /** 여기서 부터 트립북 프로젝트
+     * -발자국 보기 페이지
+     */
+    //app.get('/app/')
+
+
+
+
+
+};
+
+
+// TODO: 자동로그인 API (JWT 검증 및 Payload 내뱉기)
+// JWT 검증 API
+// app.get('/app/auto-login', jwtMiddleware, user.check);
+
+// TODO: 탈퇴하기 API
+
+   
