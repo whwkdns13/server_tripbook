3da34c00f54003e922703ff0917b8a45cda892c4
Jneck <91073404+Jneck@users.noreply.github.com>
2022-01-29 15:17:30 +0900

    Update courseDao.js
    주석추가

--- src/app/Course/courseDao.js
+++ src/app/Course/courseDao.js
@@ -1,6 +1,6 @@
 
 // 새롭게 추가한 함수를 아래 부분에서 export 해줘야 외부의 Provider, Service 등에서 사용가능합니다.
-
+// courseIdx로 모든 course정보 받아서 배열로 리턴해주는 함수
 async function selectCourseIdx(connection, courseIdx) {
     const selectCourseIdxQuery = `
                    SELECT * 
