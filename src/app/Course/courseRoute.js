4a000e8cd80e5d867369a32ba6ec720534f5252b
Jneck <91073404+Jneck@users.noreply.github.com>
2022-01-29 15:15:37 +0900

    Update courseRoute.js
    주석제거

--- src/app/Course/courseRoute.js
+++ src/app/Course/courseRoute.js
@@ -17,11 +17,4 @@ module.exports = function(app){
 
 };
 
-
-// TODO: 자동로그인 API (JWT 검증 및 Payload 내뱉기)
-// JWT 검증 API
-// app.get('/app/auto-login', jwtMiddleware, user.check);
-
-// TODO: 탈퇴하기 API
-
    
