4971de5bdcfd7951129d38797a40f87b4d8e266a
Jneck <91073404+Jneck@users.noreply.github.com>
2022-01-29 15:15:58 +0900

    Update courseProvider.js
    주석제거

--- src/app/Course/courseProvider.js
+++ src/app/Course/courseProvider.js
@@ -11,5 +11,5 @@ exports.retrieveCourse = async function (courseIdx) {
   
     connection.release();
 
-    return courseResult[0]; // 한 명의 유저 정보만을 불러오므로 배열 타입을 리턴하는 게 아닌 0번 인덱스를 파싱해서 오브젝트 타입 리턴
+    return courseResult[0];
   };
